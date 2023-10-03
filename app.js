062a596b2e209c809cfd5b9a0ae25ede8d91a29b
Kugesz <kovacsgergo.algyo@gmail.com>
2023-10-03 09:19:54 +0200

    Minden csudi szuper :)))))

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("gomb").addEventListener("change", function () {
+        if (this.checked) {
+          console.log("Checkbox is checked");
+          document.documentElement.style.setProperty("--main-color", "#060725");
+          document.documentElement.style.setProperty("--bg-color", "#111344");
+          document.documentElement.style.setProperty("--font-color", "#F0EBD8");
+        } else {
+          console.log("Checkbox is unchecked");
+          document.documentElement.style.setProperty("--main-color", "#748CAB");
+          document.documentElement.style.setProperty("--bg-color", "#F0EBD8");
+          document.documentElement.style.setProperty("--font-color", "#111344");
+        }
+      });
+});
